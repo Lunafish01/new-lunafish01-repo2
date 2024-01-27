07bf85a5d0ca0420610df30382b06a2464fb5f03
Lunafish01 <142092354+Lunafish01@users.noreply.github.com>
2024-01-26 20:54:16 -0600

    set up AboutMe and made edits to file names, edited App.jsx

--- new-lunafish01-repo2/src/App.jsx
+++ new-lunafish01-repo2/src/App.jsx
@@ -1,33 +1,31 @@
-// import { useState } from 'react'
-// import './App.css'
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import AboutMe from "./components/AboutMe";
+import ContactMe from "./components/ContactMe";
+import Portfolio from "./components/Portfolio";
+import Navbar from "./components/Navbar";
+import "./App.css";
 
-// function App() {
-//   const [count, setCount] = useState(0)
+function App() {
+  const [currentPage, setCurrentPage] = useState("AboutMe");
 
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
+  const renderPage = () => {
+    if (currentPage === "AboutMe") return <AboutMe />;
+    if (currentPage === "ContactMe") return <ContactMe />;
+    if (currentPage === " Portfolio") return <Portfolio />;
+  };
 
-// export default App
+  return (
+    <>
+      <div className="">
+        <Header />
+        <Navbar setCurrentPage={setCurrentPage} currentPage={currentPage} />
+        {renderPage()}
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ new-lunafish01-repo2/src/components/AboutMe.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function AboutMe() {
+  return (
+    <section className="" id="">
+      <h2 className="">About Me</h2>
+      <p>
+        This is my professional Junior Web Develeopment Portfolio! Im a new Web
+        Developer on my way to completing the UTSA Web Developemnt Bootcamp. I
+        will showcase some of my best work and contiue to add to this portfolio
+        as I grow as a developer.
+        <br></br>
+        Im 36 years old and currently reside in San Antonio, TX. Over the
+        course of my Web Development journey I have learned that I enjoy backend
+        development and databases the most. I am currently looking for my first
+        Junior Web Develeopment position and/or internship to kick start my career. Right
+        now I work for Spotify doing markeing consulting for SMB. With 10+ years in sales
+        and marketing, I believe I have an advantage in Web Development when it
+        comes to communitcating to the client effectively. Please do not be
+        afraid to reach me either by phone or email for any available positions.
+      </p>
+    </section>
+  );
+}
+
+export default AboutMe;
